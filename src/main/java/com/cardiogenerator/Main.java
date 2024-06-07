e48f0c7c170a2775c0c5c2ce9bc75fc88faa18ec
MaximianPopa <maximian.popa@student.maastrichtuniversity.nl>
2024-06-07 17:42:06 +0200

    Fixed Main Class

--- src/main/java/com/cardiogenerator/Main.java
+++ src/main/java/com/cardiogenerator/Main.java
@@ -7,9 +7,27 @@ import java.io.IOException;
 public class Main {
   public static void main(String[] args) throws IOException {
     if (args.length > 0 && args[0].equals("DataStorage")) {
-      DataStorage.main(new String[]{});
+      DataStorage.main(tail(args));
     } else {
-      HealthDataSimulator.main(new String[]{});
+      HealthDataSimulator.main(tail(args));
     }
   }
+
+  private static String[] tail(String[] args){
+    if (args.length == 0)
+    {
+      return args;
+    }
+
+    int length = args.length - 1;
+    String[] tail = new String[length];
+
+    for(int i = 0; i < length; i++)
+    {
+      tail[i] = args[i + 1];
+    }
+    return tail;
+  }
+
+
 }
